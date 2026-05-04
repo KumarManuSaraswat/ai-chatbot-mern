4181c6df60c055d9d6f7dc5cd4e7a3fe5fc6c88e
KumarManuSaraswat <kumar.saraswat.s.125@kalvium.community>
2026-05-04 17:05:12 +0530

    fix/CORS in server.js

--- server/server.js
+++ server/server.js
@@ -11,8 +11,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Server is running");

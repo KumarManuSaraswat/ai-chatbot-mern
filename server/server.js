7bb457fcca9b00b28703a9007ddef87aa5cddedb
KumarManuSaraswat <kumar.saraswat.s.125@kalvium.community>
2026-05-05 09:08:54 +0530

    fix/backend deployment

--- server/server.js
+++ server/server.js
@@ -11,14 +11,22 @@ connectDB();
 
 const app = express();
 
+const allowedOrigins = [process.env.CLIENT_URL, "http://localhost:5173"];
+
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
-app.use(express.json());
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running");
